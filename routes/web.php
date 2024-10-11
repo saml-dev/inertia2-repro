21a2fa3d23f34ce92e56438d895ed312facfcef8
Sam Lewis <sam.lewis0602@gmail.com>
2024-10-10 22:52:11 -0400

    remove distraction

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,4 @@ Route::get('/page2', function () {
   ]);
 })->name('page2');
 
-Route::middleware('auth')->group(function () {
-  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__ . '/auth.php';
